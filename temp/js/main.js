8e2fc18bbbec6df6f441d2ede7899d50451cd0bf
Victor Marquez <victormarquez.dev@gmail.com>
2015-04-03 17:52:19 -0400

    Updates

--- temp/js/main.js
+++ temp/js/main.js
@@ -19,37 +19,6 @@ $(document).ready(function() {
 		$('.menu-icon').children('i').toggleClass('click-fadeout');
 	});
 
-	// PROFILE TOGGLE
-	$('.navigation-header-switch').on('click', function(){
-
-		var 	switchHeight = $('#profileSwitch').height();
-			switchHeight = switchHeight + 58;
-
-		if($(this).hasClass('active')){
-
-			var 	profileHeight = 52;
-				i = 0;
-
-			$(this).parent('#profileSwitch').children('.navigation-header-switch').each(function() {
-				i++;
-			});
-
-			if(switchHeight < 59){
-				$('#profileSwitch').height((profileHeight*i)-52);
-			} else if(switchHeight > 58) {
-				$('#profileSwitch').height(0);
-			}
-
-		} else {
-
-			$(this).siblings('.navigation-header-switch').removeClass('active');
-			$(this).addClass('active');
-			$('#profileSwitch').height(58);
-
-		}
-
-	});
-
 	// Typed Effect
 
 	$('#search').focus(function(event) {
