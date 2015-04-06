44c7c3e52e14af0108858f25d2dbb09196575af5
Victor Marquez <victormarquez.dev@gmail.com>
2015-04-06 10:01:43 -0400

    Update

--- temp/js/main.js
+++ temp/js/main.js
@@ -1,12 +1,13 @@
 $(document).ready(function() {
 
-	// MENU FUNCTIONALITY
+	// menu functionality
 	$('.menu-icon, .overlay').on('click', function(){
 		$('#application').toggleClass('active-menu');
 		$('.menu-icon').children('i').toggleClass('click-fadein');
 		$('.menu-icon').children('i').toggleClass('click-fadeout');
 	});
 
+	// touch gestures
 	$('#overlay, #panelNav').on('swipeleft',function (e){
 		$('#application').removeClass('active-menu');
 		$('.menu-icon').children('i').toggleClass('click-fadein');
